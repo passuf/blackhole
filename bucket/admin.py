162fa0a933b39b16f8f51de5e4d2b7d3095327cb
passuf <passuf@gmail.com>
2017-07-24 12:34:23 +0200

    [NEW] Bulk update comments

--- bucket/admin.py
+++ bucket/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin.helpers import ActionForm
+from django import forms
 
 from .models import Bucket, Request
 
@@ -10,6 +12,22 @@ class BucketAdmin(admin.ModelAdmin):
     search_fields = ['id', 'title', 'description']
 
 
+class AddCommentForm(ActionForm):
+    comment = forms.CharField(required=False, max_length=500)
+
+
+def add_comment(modeladmin, request, queryset):
+    comment = request.POST['comment']
+    for r in queryset.all():
+        current_comment = r.comments if r.comments else ''
+        r.comments = current_comment + comment
+        r.save()
+
+
+def clear_comment(modeladmin, request, queryset):
+    queryset.update(comments="")
+
+
 class RequestAdmin(admin.ModelAdmin):
     readonly_fields = ['id', 'headers', 'method', 'host', 'remote_address', 'path', 'query_strings', 'body',
                        'response_code', 'http_referer', 'form_data', 'cookies', 'error', 'created_at', 'modified_at']
@@ -17,8 +35,9 @@ class RequestAdmin(admin.ModelAdmin):
     list_filter = ['method', 'response_code', 'created_at', 'modified_at', 'bucket__title']
     search_fields = ['id', 'headers', 'host', 'remote_address', 'path', 'query_strings', 'body', 'form_data', 'cookies',
                      'error', 'comments']
-
     ordering = ('-created_at',)
+    action_form = AddCommentForm
+    actions = [add_comment, clear_comment]
 
 admin.site.register(Bucket, BucketAdmin)
 admin.site.register(Request, RequestAdmin)
