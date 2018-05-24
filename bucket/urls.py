ea89c469127263be2123849f1f6d592b5cd5ded7
Jonas Passerini <jonas@futurae.com>
2018-05-24 15:32:16 +0200

    added view for SMS demo

--- bucket/urls.py
+++ bucket/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<bucket_uuid>[0-9a-f-]+)/$', views.add_request, name='add_request'),
+    url(r'^(?P<bucket_uuid>[0-9a-f-]+)/show$', views.show_requests, name='add_request'),
 ]
--- bucket/views.py
+++ bucket/views.py
@@ -1,6 +1,9 @@
+import json
+
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 from .models import Bucket, RequestFactory
 
@@ -11,3 +14,17 @@ def add_request(request, bucket_uuid):
     req = RequestFactory.from_request(request, bucket)
 
     return HttpResponse('OK', status=req.response_code if req else 400)
+
+
+@login_required
+def show_requests(request, bucket_uuid):
+    bucket = get_object_or_404(Bucket, pk=bucket_uuid, active=True)
+    requests = []
+    for r in bucket.requests.all():
+        try:
+            form_data = json.loads(r.form_data)
+            if form_data:
+                requests.append(form_data)
+        except Exception:
+            pass
+    return render(request, 'bucket/show.html', {'bucket': bucket, 'requests': requests})
